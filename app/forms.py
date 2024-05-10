dae85a11739217ee897e515b13d0919375f627ea
Mojalefa Kodisang <kodisangmojalefa65@gmail.com>
2024-05-11 01:54:28 +0200

    [Update] - Made an Account route, user update info

--- app/forms.py
+++ app/forms.py
@@ -1,20 +1,16 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
+from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from app.model import User
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField("Username", validators=[
-                                                    DataRequired(),
-                                                    Length(min=2, max=20)])
-    email = StringField("Email", validators=[
-                                                    DataRequired(),
-                                                    Email()])
+    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
-    confirm_password = PasswordField("Confirm Password", validators=[
-                                                    DataRequired(),
-                                                    EqualTo('password')])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField("Sign Up")
     
     def validate_username(self, username):
@@ -36,3 +32,21 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     remember = BooleanField("Remember Me")
     submit = SubmitField("Sign In")
+    
+class UpdateAccountForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField("Update")
+    
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username already taken. Please suggest a new one')
+    
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email already taken. Please suggest a new one')
